1b8d477c2b223a5f845170b80df0558842818624
Mario Fernandez <31100373+mariof77@users.noreply.github.com>
2017-09-03 22:17:10 -0300

    Landing
    35 de 64 elementos completado

--- Landing/js/main.js
+++ Landing/js/main.js
@@ -3,6 +3,12 @@ $(document).ready(function() {
 
     var nav = $('.navbar-fixed-top');
 
+    var distancia = $('.navbar-fixed-top').offset();
+
+    if(distancia.top >0){
+        nav.removeClass('normal').addClass('efecto');
+    }
+
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
 
@@ -18,19 +24,19 @@ $(document).ready(function() {
 	});
 
 	$('section#app .anima1').waypoint(function() {
-		$('section#app .anima1').addClass('animated rubberBand');
+		$('section#app .anima1').addClass('animated rollIn');
 	}, {
 		offset: '50%'
 	});
 
 	$('section#app .anima2').waypoint(function() {
-		$('section#app .anima2').addClass('animated rubberBand');
+		$('section#app .anima2').addClass('animated rollIn');
 	}, {
 		offset: '50%'
 	});
 
 	$('section#app .anima3').waypoint(function() {
-		$('section#app .anima3').addClass('animated rubberBand');
+		$('section#app .anima3').addClass('animated rollIn');
 	}, {
 		offset: '50%'
 	});
@@ -88,8 +94,53 @@ $(document).ready(function() {
 		offset: '60%'
 	});
 
+    $('#soporteForm').bootstrapValidator({
+        message: "Esto no es válido",
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            nombre: {
+                validators: {
+                    notEmpty: {
+                        message: "Este es un campo obligatorio"
+                    },
+                }
+            },
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: "Este es un campo obligatorio"
+                    },                
+                    emailAddress: {                        
+                        message: "Esto no parece un correo electrónico"
+                    }
+                }
+            },
+            mensaje: {
+                validators: {
+                    notEmpty: {
+                        message: "El mensaje no puede ir vacio"
+                    }
+                }
+            }
+        }
+    }).on('success.form.bv', function(e) {
+        e.preventDefault();
+        var $form = $(e.target);
 
-   
+        var bv = $form.data('bootstrapValidator');
+
+        $.post($form.attr('action'),$form.serialize(), function(result) {
+            $('#correcto').fadeIn();
+            console.log(result);
+        }), 'json';
+
+    });
+    
+    
 });
 
 // https://www.diycode.cc/projects/dimsemenov/smooth-scroll
